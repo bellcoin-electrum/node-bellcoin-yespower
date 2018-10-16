65ea9370a8b5c637e325e5c80d923fd9e6b5484d
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-10-17 00:01:08 +0900

    fix bellcoin_yespower

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-//var bitzeny_yescrypt = require('./build/Release/bitzeny_yescrypt');
-//var bitzeny_yescrypt = require('bindings')('bitzeny_yescrypt');
-var bitzeny_yescrypt = require('../node-bitzeny-yescrypt');
+//var bellcoin_yespower = require('./build/Release/bellcoin_yespower');
+//var bellcoin_yespower = require('bindings')('bellcoin_yespower');
+var bellcoin_yespower = require('../node-bellcoin-yespower');
 
 var data = new Buffer("7000000001e980924e4e1109230383e66d62945ff8e749903bea4336755c00000000000051928aff1b4d72416173a8c3948159a09a73ac3bb556aa6bfbcad1a85da7f4c1d13350531e24031b939b9e2b", "hex");
-console.log(bitzeny_yescrypt.hash(data).toString('hex'));
+console.log(bellcoin_yespower.hash(data).toString('hex'));
 // result: 91aed7746ff09fa6b475c5da2672f0150508c2b90bc999a8400047feeb5b9c1e
 
