3aabdd42bc4aeca2e039b58c7f51227594f4b1e2
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-10-16 23:56:39 +0900

    fix bellcoin

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-var use_yespower = 1;
-
-if(!use_yespower) {
-    module.exports = require('bindings')('bitzeny_yescrypt');
-} else {
-    module.exports = require('bindings')('bitzeny_yespower');
-}
+module.exports = require('bindings')('bellcoin_yespower');
