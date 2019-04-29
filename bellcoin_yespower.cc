8997d466706c896eedb0328a715b7405dd8001ae
y-chan <34832037+y-chan@users.noreply.github.com>
2019-04-29 11:22:08 +0900

    fix yespower

--- bellcoin_yespower.cc
+++ bellcoin_yespower.cc
@@ -1,39 +1,41 @@
 #include <node.h>
 #include <node_buffer.h>
-#include <v8.h>
 #include <nan.h>
 
-extern "C" {
 #include "yespower-1.0.0/yespower.h"
-}
 
 #define THROW_ERROR_EXCEPTION(x) Nan::ThrowError(x)
 
-using namespace node;
-using namespace v8;
+using v8::Exception;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
 
-NAN_METHOD(hash) {
-    if (info.Length() < 1) {
-        return THROW_ERROR_EXCEPTION("You must provide one argument.");
+void hash(const Nan::FunctionCallbackInfo<Value>& args) {
+
+    if(args.Length() < 1) {
+        Nan::ThrowTypeError("1 argument expected");
+        return;
+    }  
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!node::Buffer::HasInstance(target)) {
+        Nan::ThrowTypeError("Function expects buffer as input");
+        return;
     }
 
-    Local<Object> target = Nan::To<Object>(info[0]).ToLocalChecked();
-
-    if(!Buffer::HasInstance(target)) {
-        return THROW_ERROR_EXCEPTION("Argument should be a buffer object.");
-    }
-
-    char *input = Buffer::Data(target);
-    char *output = (char*) malloc(sizeof(char) * 32);
+    char* input = node::Buffer::Data(target);
+    char* output = new char[32];
 
     yespower_hash(input, output);
 
-    info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
+    args.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
 }
 
-NAN_MODULE_INIT(init)
-{
-    NAN_EXPORT(target, hash);
+void Init(v8::Local<v8::Object> exports) {
+    exports->Set(Nan::New("hash").ToLocalChecked(), Nan::New<v8::FunctionTemplate>(hash)->GetFunction());
 }
 
-NODE_MODULE(bellcoin_yespower, init);
+NODE_MODULE(bellcoin_yespower, Init);
